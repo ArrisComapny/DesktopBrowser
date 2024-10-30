05f8709d10b0f50de01b1cc0d4152e3c4f558f07
Droid216 <bit216den@gmail.com>
2024-10-30 21:07:52 +0500

    update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,14 @@ if [[ -z "$VIRTUAL_ENV" ]]; then
     exit 1
 fi
 
-pyinstaller my.spec
+libc_path=$(find /usr /Library /opt -name "libc++.dylib" 2>/dev/null | head -n 1)
+
+if [[ -z "$libc_path" ]]; then
+    echo "Ошибка: библиотека libc++.dylib не найдена."
+    exit 1
+fi
+
+pyinstaller --paths "$libc_path" my.spec
 
 deactivate
 
