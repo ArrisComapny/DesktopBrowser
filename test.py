e96a6eda1e4eb1a0e9959bde1108044a94f7683b
Droid216 <bit216den@gmail.com>
2024-10-31 16:48:37 +0500

    create test.py

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-# test_blinker.py
-from blinker import signal
+from seleniumwire import webdriver
+from mitmproxy import proxy, options
 
-def handler(sender):
-    print(f"Received signal from {sender}")
-
-my_signal = signal('test')
-my_signal.connect(handler)
-my_signal.send('test_sender')
+print("Импорты выполнены успешно.")
