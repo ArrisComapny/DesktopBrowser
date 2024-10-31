44516558f27e5df1382489d2dc2c0c549641a2b5
Droid216 <bit216den@gmail.com>
2024-11-01 01:43:24 +0500

    update for macos

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import json
-import threading
 import webbrowser
 
 import pyautogui
@@ -16,17 +15,10 @@ from selenium.common.exceptions import NoSuchWindowException, WebDriverException
 
 from database.db import DbConnection
 
-# if hasattr(sys, '_MEIPASS'):
-#     icon_path = os.path.join(sys._MEIPASS, 'chrome.png')
-# else:
-#     icon_path = os.path.join(os.getcwd(), 'chrome.png')
-
 
 class WebDriver:
     def __init__(self, phone: str, proxy: str):
-
         self.browser_id = phone
-
         self.profile_path = os.path.join(os.getcwd(), f"chrome_profile/chrome_profile_{phone}")
         os.makedirs(self.profile_path, exist_ok=True)
 
@@ -40,7 +32,6 @@ class WebDriver:
         }
 
         self.chrome_options = uc.ChromeOptions()
-
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-gpu")
         self.chrome_options.add_argument("--disable-extensions")
@@ -52,18 +43,15 @@ class WebDriver:
         self.chrome_options.add_experimental_option("useAutomationExtension", False)
         self.chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         self.chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        self.chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                                         "(KHTML, like Gecko) Chrome/119.0.5945.86 Safari/537.36")
+        self.chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.5945.86 Safari/537.36")
 
         try:
             self.service = Service(ChromeDriverManager().install())
-
             self.driver = webdriver.Chrome(service=self.service,
                                            options=self.chrome_options,
                                            seleniumwire_options=self.proxy_options)
             self.driver.maximize_window()
         except WebDriverException as e:
-
             raise e
 
     def is_browser_active(self):
@@ -90,13 +78,9 @@ class WebDriver:
 
 
 class BrowserApp(QtWidgets.QWidget):
-    browser_loaded = QtCore.pyqtSignal(bool)
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MarketBrowser")
-
-        # self.setWindowIcon(QtGui.QIcon(icon_path))
 
         screen_width, screen_height = pyautogui.size()
         x_position = (screen_width - 400) // 2
@@ -108,12 +92,9 @@ class BrowserApp(QtWidgets.QWidget):
         self.db_conn = DbConnection()
         self.markets = self.db_conn.info()
 
-        self.browser_loaded.connect(self.on_browser_loaded)
-
         self.init_ui()
 
     def init_ui(self):
-
         marketplaces = sorted(list({m.marketplace for m in self.markets}))
 
         self.marketplace_select = QtWidgets.QComboBox()
@@ -133,31 +114,20 @@ class BrowserApp(QtWidgets.QWidget):
         layout = QtWidgets.QVBoxLayout()
         layout.addLayout(form_layout)
         layout.addWidget(self.launch_button)
-        self.launch_button.setEnabled(True)
-
         self.setLayout(layout)
 
     def update_markets(self):
         selected_marketplace = self.marketplace_select.currentText()
-
         filtered_companies = sorted([m.name_company for m in self.markets if m.marketplace == selected_marketplace])
-
         self.market_select.clear()
         self.market_select.addItems(filtered_companies)
 
     def launch_browser(self):
         self.launch_button.setEnabled(False)
         self.launch_button.setText('Загружаю браузер...')
-        threading.Thread(target=self.launch_browser_thread, daemon=True).start()
-        # QtCore.QTimer.singleShot(0, self.launch_browser_thread)
-
-    def launch_browser_thread(self):
         marketplace = self.marketplace_select.currentText()
         name_company = self.market_select.currentText()
-
         market = self.db_conn.get_market(marketplace=marketplace, name_company=name_company)
-
-        self.cleanup_inactive_drivers()
 
         for driver in self.web_drivers:
             if driver.browser_id == market.connect_info.phone:
@@ -169,20 +139,10 @@ class BrowserApp(QtWidgets.QWidget):
                 self.web_drivers.append(web_driver)
                 web_driver.load_url(market.marketplace_info.link)
             except WebDriverException:
-                self.browser_loaded.emit(False)
-                return
-
-        self.browser_loaded.emit(True)
-
-    def on_browser_loaded(self, success):
-        if not success:
-            QtWidgets.QMessageBox.critical(None, "Ошибка", "Не удалось запустить Chrome. Пожалуйста, установите его.")
-            webbrowser.open("https://www.google.com/chrome/")
+                QtWidgets.QMessageBox.critical(None, "Ошибка", "Не удалось запустить Chrome. Пожалуйста, установите его.")
+                webbrowser.open("https://www.google.com/chrome/")
         self.launch_button.setEnabled(True)
         self.launch_button.setText("Запуск браузера")
-
-    def cleanup_inactive_drivers(self):
-        self.web_drivers = [driver for driver in self.web_drivers if driver.is_browser_active()]
 
     def closeEvent(self, event):
         for driver in self.web_drivers:
@@ -190,27 +150,10 @@ class BrowserApp(QtWidgets.QWidget):
         event.accept()
 
 
-class LoginWorker(QtCore.QThread):
-    login_checked = QtCore.pyqtSignal(bool, str, str)
-
-    def __init__(self, db_conn, login, password):
-        super().__init__()
-        self.db_conn = db_conn
-        self.login = login
-        self.password = password
-
-    def run(self):
-        is_valid_user = self.db_conn.check_user(login=self.login, password=self.password)
-        self.login_checked.emit(is_valid_user, self.login, self.password)
-
-
 class LoginWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Авторизация")
-
-        # self.setWindowIcon(QtGui.QIcon(icon_path))
-
         self.credentials_file = 'credentials.json'
         self.db_conn = None
         self.key = None
@@ -223,34 +166,23 @@ class LoginWindow(QtWidgets.QWidget):
         self.init_ui()
 
         self.connect_to_db()
-        # threading.Thread(target=self.connect_to_db, daemon=True).start()
 
     def init_ui(self):
         form_layout = QtWidgets.QFormLayout()
-
         self.login_input = QtWidgets.QLineEdit(self)
         self.password_input = QtWidgets.QLineEdit(self)
         self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
-
         form_layout.addRow("Логин:", self.login_input)
         form_layout.addRow("Пароль:", self.password_input)
 
         self.remember_me_checkbox = QtWidgets.QCheckBox("Запомнить", self)
-
-        self.login_button = QtWidgets.QPushButton("Подключение...", self)
+        self.login_button = QtWidgets.QPushButton("Войти", self)
         self.login_button.clicked.connect(self.check_login)
-        self.login_button.setEnabled(False)
-
-        self.login_button.setDefault(True)
-
-        self.login_input.returnPressed.connect(self.login_button.click)
-        self.password_input.returnPressed.connect(self.login_button.click)
 
         main_layout = QtWidgets.QVBoxLayout()
         main_layout.addLayout(form_layout)
         main_layout.addWidget(self.remember_me_checkbox)
         main_layout.addWidget(self.login_button)
-
         self.setLayout(main_layout)
 
     def connect_to_db(self):
@@ -258,29 +190,15 @@ class LoginWindow(QtWidgets.QWidget):
             self.db_conn = DbConnection()
             self.key = self.db_conn.get_key()
             self.load_credentials()
-            self.login_button.setText("Войти")
             self.login_button.setEnabled(True)
-
         except Exception as e:
-            print(e)
-            self.loading_dialog.close()
             QtWidgets.QMessageBox.critical(self, "Ошибка", f"Не удалось подключиться к БД: {str(e)}")
             self.close()
 
     def check_login(self):
-        self.login_button.setText("Проверка...")
-        self.login_button.setEnabled(False)
-
         login = self.login_input.text()
         password = self.password_input.text()
-        self.worker = LoginWorker(self.db_conn, login, password)
-        self.worker.login_checked.connect(self.update_ui_after_login)
-        self.worker.start()
-
-    def update_ui_after_login(self, is_valid_user, login, password):
-        self.login_button.setText("Войти")
-        self.login_button.setEnabled(True)
-
+        is_valid_user = self.db_conn.check_user(login=login, password=password)
         if is_valid_user:
             if self.remember_me_checkbox.isChecked():
                 self.save_credentials(login, password)
