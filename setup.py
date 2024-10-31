bd9d418b6198014df1b728043e6dfa2d84e342c4
Droid216 <bit216den@gmail.com>
2024-11-01 03:26:05 +0500

    update for macos

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup
+import os
 
 APP = ['main.py']  # Замените 'main.py' на основной файл вашего приложения
 DATA_FILES = []    # Укажите дополнительные файлы, такие как иконки и конфигурации, если нужны
+
+
+# Читаем зависимости из requirements.txt и добавляем их в packages
+def get_packages():
+    packages = []
+    if os.path.exists("requirements-mac.txt"):
+        with open("requirements-mac.txt", "r") as f:
+            for line in f:
+                # Берем только название пакета, убирая версии и лишние символы
+                package_name = line.strip().split("~=")[0].split("==")[0]
+                if package_name and package_name not in ["pyinstaller", "py2app"]:
+                    packages.append(package_name)
+    return packages
+
+
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['PyQt5', 'undetected_chromedriver', 'seleniumwire', 'cryptography', 'sqlalchemy'],
-    'includes': ['psutil'],  # Укажите здесь все необходимые модули
+    'packages': get_packages(),
     'excludes': ['tkinter'],  # Исключите ненужные модули
     'plist': {
         'CFBundleName': 'ProxyBrowser',          # Имя приложения
